ec753642deef4b73d797237eeffa86c7b5d4d536
sandrasianto <sandra.ntikoisa@strathmore.edu>
2020-01-26 14:14:03 +0300

    hello toast1 code

---
+++ app/src/main/java/com/example/hellotoast1/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.hellotoast1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void countUp(View view) {
+    }
+
+    public void showToast(View view) {
+    }
+}
